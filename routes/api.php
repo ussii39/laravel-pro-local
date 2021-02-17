121e07a57dd3560c3956d5ce95bba0719b7e38fc
ussii39 <usimarutetunori3@outlook.jp>
2021-02-18 00:20:41 +0900

    route controller name

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users','App\Http\Controllers\UsersController@index');
+Route::get('/users','UsersController@index');
