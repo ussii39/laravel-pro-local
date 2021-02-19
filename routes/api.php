b77591a2cabfe989f9193adbf2333c06e399f8ed
ussii39 <usimarutetunori3@outlook.jp>
2021-02-19 13:00:09 +0900

    first commit

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users','UsersController@index');
+   Route::middleware(['cors'])->group(function () {
+    Route::get('/users','UsersController@index');
+});
